49c87b57d574621e938a92b65717f4da175d60e4
mockingjay <8400684@qq.com>
2024-09-09 09:39:55 +0800

    Initial commit

---
+++ win7aot/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace win7aot.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ win7aot/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace win7aot.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
